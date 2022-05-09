0aa1d5712f11ecb209912bebfdef4589c22f0c5d
yousef.omd <yousefomidali78@gmail.com>
2022-05-09 20:33:51 +0430

    Application.java, pom, application.properties and some other unnecessary files

---
+++ src/main/java/org/maktab/Part2MaktabFinalProject/Part2MaktabFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package org.maktab.Part2MaktabFinalProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Part2MaktabFinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Part2MaktabFinalProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/maktab/Part2MaktabFinalProject/Part2MaktabFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.maktab.Part2MaktabFinalProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Part2MaktabFinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
